b2bc61d4622d3411fba0b58e2cab77eaef9c693a
Jason Hu <jasonhu324@gmail.com>
2022-07-03 21:10:12 +0800

    backend can get params

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_cors import cross_origin
 from flask import jsonify
 
@@ -7,13 +7,17 @@ app = Flask(__name__)
 
 
 @app.route('/test/',methods=['POST','GET'])
-@cross_origin(origins="http://127.0.0.1:3000") # 设置可以访问的前端端口
+# @cross_origin(origins="http://127.0.0.1:9528") # 设置可以访问的前端端口
 def test():
     print("receive")
-    json = request.json
-    print('recv',json)
-    print('recv',json['name'])
-    return jsonify({'a':'aprime','b':'bprime'})
+    data = request.form.get('')
+    print(data)
+    response = make_response(jsonify({'a':'aprime','b':'bprime'}))
+    response.headers["Access-Control-Allow-Origin"] = 'http://127.0.0.1:9528'	# 允许使用响应数据的域。也可以利用请求header中的host字段做一个过滤器。
+    response.headers["Access-Control-Allow-Methods"] = 'POST,GET'	# 允许的请求方法
+    response.headers["Access-Control-Allow-Headers"] = 'x-requested-with,content-type'	# 允许的请求header
+    response.headers["Access-Control-Allow-Credentials"] = 'true'
+    return response
 
 
 if __name__ == '__main__':
