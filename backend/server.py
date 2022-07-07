8befacb63dce33edc954ee9dec219d354c6fa72f
Jason Hu <jasonhu324@gmail.com>
2022-07-07 13:22:39 +0800

    create function uplodFilepathToDB

--- backend/server.py
+++ backend/server.py
@@ -114,6 +114,19 @@ def uploadFile():
     return response
 
 
+@app.route('/uploadfile', methods=['GET', 'POST'])
+# @cross_origin(origins="http://127.0.0.1:3000") # 设置可以访问的前端端口
+def uploadFilepathToDB():
+    print('---- Upload Filepath to DB ----')
+    
+    response = make_response(jsonify({}))
+    response.headers["Access-Control-Allow-Origin"] = 'http://localhost:9528'	# 允许使用响应数据的域。也可以利用请求header中的host字段做一个过滤器。
+    response.headers["Access-Control-Allow-Methods"] = 'POST,GET'	# 允许的请求方法
+    response.headers["Access-Control-Allow-Headers"] = 'x-requested-with,content-type'	# 允许的请求header
+    response.headers["Access-Control-Allow-Credentials"] = 'true'
+    print('---- ----- ----')
+    return response
+
 @app.route('/download', methods=['GET', 'POST'])
 # @cross_origin(origins="http://127.0.0.1:3000") # 设置可以访问的前端端口
 def download():
